9eaa510390843f17a64ace5b67b9d0b6ac166fe9
RayMaroun <50945743+RayMaroun@users.noreply.github.com>
2023-11-16 22:41:49 +0200

    Added the employee class (#1)

---
+++ src/main/java/com/pluralsight/Employee.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+public class Employee extends Person{
+
+    private double salary;
+
+    public Employee(String firstName, String lastName, int age, double salary) {
+        super(firstName, lastName, age);
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
